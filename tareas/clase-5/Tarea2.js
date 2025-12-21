18e845e23628443f8f9ea6324632bc3ec35ffa41
NicoTrusz <nicolastrusz@gmail.com>
2025-12-21 15:34:23 -0300

    Tarea 5

---
+++ tareas/clase-5/Tarea2.js
@@ -0,0 +1,15 @@
+console.log('Tarea2');
+
+const $Guardar = document.querySelector('#Guardar');
+$Guardar.onclick = function(){
+  const primerNombre = document.querySelector('#primer-nombre').value;
+  const segundoNombre = document.querySelector('#segundo-nombre').value;
+  const apellido = document.querySelector('#apellido').value;
+  const edad = document.querySelector('#Edad').value;
+  const informacionCompleta = `${primerNombre} ${segundoNombre} ${apellido}, Edad: ${edad}`;
+  document.querySelector('#resultado').innerText = informacionCompleta;
+  const $h1 = document.querySelector('h1');
+  $h1.innerText = `Bienvenido, ${primerNombre}!`;
+  console.log(informacionCompleta);
+  return false;
+}
